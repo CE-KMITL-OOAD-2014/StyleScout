1ba3a7d0d7d35569f6ec05de7a46363c6286e56a
Stylescout <yanisa.nach@gmail.com>
2014-10-29 22:54:13 +0700

    m3

---
+++ myphp_test/deletetest.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'deletepic.php';
+include '../delete.html';
+
+foreach (scandir(dirname(_FILE_).'/delete')as $filename){
+	$path = dirname(_FILE_).'/delete/' . $filename;
+	if(is_file($path)){
+		require_once $path;
+	}
+}
+class deletetest extends PHPUnit_Framework_TestCase
+		public $test;
+{
+    public function tesITCanDeletePic()
+    {
+        $Delete = new deletepic();
+		
+		$clickbutton = new clickButton();
+		$dummypicture = $this->getMock('PICS');
+		$this->assrtTrue($Delete->deletingPicture($clickutton,$dummypicture));
+    }
+	
+}
+?>
---
+++ myphp_test/uploadtest.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'uploadpic.php';
+include '../upload.html';
+
+foreach (scandir(dirname(_FILE_).'/upload')as $filename){
+	$path = dirname(_FILE_).'/upload/' . $filename;
+	if(is_file($path)){
+		require_once $path;
+	}
+}
+class uploadtest extends PHPUnit_Framework_TestCase
+		public $test;
+{
+    public function tesITCanUploadPic()
+    {
+        $Upload = new uploadpic();
+		
+		$clickbutton = new clickButton();
+		$dummypicture = $this->getMock('PICS');
+		$this->assrtTrue($Upload->uploadingPicture($clickutton,$dummypicture));
+    }
+	
+}
+?>
